89a9f5ef54a59901a330cf6d74183cf18e338dd6
Hans Jerry Illikainen <hji@dyntopia.com>
2024-05-09 08:03:36 +0000

    main: optionally wait for a debugger to attach

--- main.go
+++ main.go
@@ -9,11 +9,16 @@ import (
 	"github.com/fatih/color"
 	"github.com/illikainen/go-utils/src/ensure"
 	"github.com/illikainen/go-utils/src/logging"
+	"github.com/illikainen/go-utils/src/sandbox"
 	"github.com/mattn/go-isatty"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	if sandbox.IsSandboxed() && sandbox.IsDebugging() {
+		sandbox.AwaitDebugger()
+	}
+
 	color.NoColor = !isatty.IsTerminal(os.Stderr.Fd())
 
 	log.SetOutput(os.Stderr)
